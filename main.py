5e053ba523b80020ede42d54864b48668b387951
Administrator <bachvkhoa@ubuntu.ubuntu-domain>
2019-01-23 16:48:43 +0700

    generate weekly image

--- main.py
+++ main.py
@@ -39,5 +39,6 @@ def test():
 @app.route('/', methods=['GET'])
 def index():
     return 'hello ^^,'
-port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port = port)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port = port)
