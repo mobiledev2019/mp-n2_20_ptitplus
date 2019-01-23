33856d2e7ac6ac87a8aac60c3c916d35760c9fea
Administrator <bachvkhoa@ubuntu.ubuntu-domain>
2019-01-23 21:57:32 +0700

    add weekly picture in reply

--- qldt_schedule_creator.py
+++ qldt_schedule_creator.py
@@ -202,6 +202,7 @@ def get_tkb_page(student_id):
     except:
         # traceback.print_exc()
         pass
+    print("CALLING img_uploader(), len = {}".format(len(img_b64)))
     img_url = img_uploader.up(img_b64)
     return rtn
 
