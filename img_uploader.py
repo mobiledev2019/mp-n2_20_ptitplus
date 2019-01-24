cffb88cc9244704a6b43c8d1343bef82b1a00794
Administrator <bachvkhoa@ubuntu.ubuntu-domain>
2019-01-24 17:32:51 +0700

    add weekly picture in reply

--- img_uploader.py
+++ img_uploader.py
@@ -4,8 +4,14 @@ import cloudinary.uploader as uploader
 def up(img):
     cloudname = 'bachvkhoa'
     preset_code = 'anhoiemup'
-    print("IMG_UPLOADER ls -> ")
-    os.system('ls')
-    rps = uploader.unsigned_upload(img, preset_code, cloud_name=cloudname)
+    print("IMG_UPLOADER listdir -> ")
+    crr_dir = os.listdir('./')
+    print(crr_dir)
+    for f in crr_dir:
+        if f[:-4] == '.jpg':
+            print('img found -> {}'.format(f))
+        else:
+            print('{} is not a jpg file'.format(f))
+    # rps = uploader.unsigned_upload(img, preset_code, cloud_name=cloudname)
     url = rps['secure_url']
     return url
