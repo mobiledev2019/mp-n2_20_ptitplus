4f56f9e216179e5e570caf186954abee4fcc5d40
Administrator <bachvkhoa@ubuntu.ubuntu-domain>
2019-01-24 17:37:08 +0700

    add weekly picture in reply

--- img_uploader.py
+++ img_uploader.py
@@ -8,10 +8,10 @@ def up(img):
     crr_dir = os.listdir('./')
     print(crr_dir)
     for f in crr_dir:
-        if f[:-4] == '.jpg':
-            print('img found -> {}'.format(f))
+        if f[-4:] == '.jpg':
+            print('[OK] img found -> {}'.format(f))
         else:
-            print('{} is not a jpg file'.format(f))
+            print('[  ]{} is not a jpg file'.format(f))
     # rps = uploader.unsigned_upload(img, preset_code, cloud_name=cloudname)
     url = rps['secure_url']
     return url
