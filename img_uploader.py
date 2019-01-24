da8b0f38c690a37f0c8d569725ccca9cc67d7d3e
Administrator <bachvkhoa@ubuntu.ubuntu-domain>
2019-01-24 17:21:57 +0700

    add weekly picture in reply

--- img_uploader.py
+++ img_uploader.py
@@ -1,9 +1,11 @@
-import requests, time, json
+import requests, time, json, os
 import cloudinary.uploader as uploader
 
 def up(img):
     cloudname = 'bachvkhoa'
     preset_code = 'anhoiemup'
+    print("IMG_UPLOADER ls -> ")
+    os.system('ls')
     rps = uploader.unsigned_upload(img, preset_code, cloud_name=cloudname)
     url = rps['secure_url']
     return url
